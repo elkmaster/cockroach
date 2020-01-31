e14ff89f74e990ea23da9c0856fd0339111146e3
Vlad Los <carrott9@gmail.com>
2020-01-31 15:42:25 +0200

    ui: statements datepicker
    updated datepicker design
    
    Resolves: #44422
    
    Release note(ui): none

---
+++ pkg/ui/assets/caret-left.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+// tslint:disable-next-line: variable-name
+const CuretLeft = () => (
+  <svg width={11} height={16} viewBox="0 0 11 16" fill="none">
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M9.704 2.968A1.286 1.286 0 008.01 1.032l-6.857 6a1.286 1.286 0 000 1.936l6.857 6a1.286 1.286 0 001.694-1.936L3.952 8l5.752-5.032z"
+      fill="#394455"
+    />
+  </svg>
+);
+
+export default CuretLeft;
---
+++ pkg/ui/assets/caret-right.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+// tslint:disable-next-line: variable-name
+const CuretRight = () => (
+  <svg width={11} height={16} viewBox="0 0 11 16" fill="none">
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M1.153 13.032a1.286 1.286 0 101.694 1.936l6.857-6a1.286 1.286 0 000-1.936l-6.857-6a1.286 1.286 0 00-1.694 1.936L6.905 8l-5.752 5.032z"
+      fill="#394455"
+    />
+  </svg>
+);
+
+export default CuretRight;
